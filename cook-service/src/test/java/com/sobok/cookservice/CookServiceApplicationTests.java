9028cf41cf004a660dab9e277cdbd0d8e8b430ad
Devy17 <devyks107@gmail.com>
2025-06-28 21:41:55 +0900

    feat : cook-service 생성 및 기초 설정

---
+++ cook-service/src/test/java/com/sobok/cookservice/CookServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sobok.cookservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CookServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
