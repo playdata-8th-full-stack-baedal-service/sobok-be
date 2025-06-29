db544f15267b30146d02fd7d2f3e17baca2dd2ff
HanJongKyun <135014484+HanJongKyun@users.noreply.github.com>
2025-06-29 16:26:13 +0900

    fix: user ReqDto 주석 수정

--- auth-service/src/main/java/com/sobok/authservice/auth/dto/request/AuthReqDto.java
+++ auth-service/src/main/java/com/sobok/authservice/auth/dto/request/AuthReqDto.java
@@ -1,7 +1,6 @@
 package com.sobok.authservice.auth.dto.request;
 
 
-import com.sobok.authservice.common.enums.Role;
 import lombok.*;
 
 @Getter
@@ -14,14 +13,11 @@ public class AuthReqDto {
 
     private String loginId;
     private String password;
-    private Role role;
-
     private String nickname;
     private String phone;
     private String email;
     private String photo;
-
-    private String roadFull;
-    private String addrDetail;
+//    private String roadFull;
+//    private String addrDetail;
 
 }
