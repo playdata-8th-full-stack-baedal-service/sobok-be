f818fdea60466c21918c31541cb7c3a6a6a27b50
hyeonji <guswl1424@gmail.com>
2025-06-30 22:15:42 +0900

    fix: user feign client 중복 수정

--- auth-service/src/main/java/com/sobok/authservice/auth/client/UserServiceClient.java
+++ auth-service/src/main/java/com/sobok/authservice/auth/client/UserServiceClient.java
@@ -1,6 +1,8 @@
 package com.sobok.authservice.auth.client;
 
+import com.sobok.authservice.auth.dto.request.UserSignupReqDto;
 import com.sobok.authservice.common.dto.ApiResponse;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,4 +16,7 @@ public interface UserServiceClient {
 
     @PostMapping("/user/findByPhoneNumber")
     ApiResponse<UserResDto> findByPhone(@RequestBody String phoneNumber);
+
+    @PostMapping("/api/signup")
+    ResponseEntity<Object> userSignup(@RequestBody UserSignupReqDto reqDto);
 }
--- auth-service/src/main/java/com/sobok/authservice/auth/feign/UserFeignClient.java
+++ auth-service/src/main/java/com/sobok/authservice/auth/feign/UserFeignClient.java
@@ -6,9 +6,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-@FeignClient(name = "user-service")
-public interface UserFeignClient  {
-    @PostMapping("/api/signup")
-    ResponseEntity<Object> userSignup(@RequestBody UserSignupReqDto reqDto);
-}
+// 일단 주석 처리 해두었습니다. 확인 후 삭제해도 됩니다.
+//@FeignClient(name = "user-service")
+//public interface UserFeignClient  {
+//    @PostMapping("/api/signup")
+//    ResponseEntity<Object> userSignup(@RequestBody UserSignupReqDto reqDto);
+//}
