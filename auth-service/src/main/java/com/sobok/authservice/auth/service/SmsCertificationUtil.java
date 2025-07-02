f0bb30cf71fc12fcfbdd1f93a9bbf000cef02de9
hyeonji <guswl1424@gmail.com>
2025-07-02 09:38:05 +0900

    fix: sendSMS 예외 처리

--- auth-service/src/main/java/com/sobok/authservice/auth/service/SmsCertificationUtil.java
+++ auth-service/src/main/java/com/sobok/authservice/auth/service/SmsCertificationUtil.java
@@ -1,5 +1,6 @@
 package com.sobok.authservice.auth.service;
 
+import com.sobok.authservice.common.exception.CustomException;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -8,6 +9,7 @@ import net.nurigo.sdk.message.model.Message;
 import net.nurigo.sdk.message.request.SingleMessageSendingRequest;
 import net.nurigo.sdk.message.service.DefaultMessageService;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -33,13 +35,16 @@ public class SmsCertificationUtil {
 
     // 단일 메시지 발송
     public void sendSMS(String phoneNumber, String certificationCode){
-        log.info("Sending SMS from {}", fromNumber);
-        log.info("phone number: {}", phoneNumber);
-        Message message = new Message(); // 새 메시지 객체 생성
-        message.setFrom(fromNumber); // 발신자 번호 설정
-        message.setTo(phoneNumber); // 수신자 번호 설정
-        message.setText("본인 확인 인증번호는 " + certificationCode + "입니다."); // 메시지 내용 설정
-
-        messageService.sendOne(new SingleMessageSendingRequest(message)); // 메시지 발송 요청
+        try {
+            log.info("phone number: {}", phoneNumber);
+            Message message = new Message(); // 새 메시지 객체 생성
+            message.setFrom(fromNumber); // 발신자 번호 설정
+            message.setTo(phoneNumber); // 수신자 번호 설정
+            message.setText("본인 확인 인증번호는 " + certificationCode + "입니다."); // 메시지 내용 설정
+            // 임시 주석. 테스트 할 시 주석 해제
+//            messageService.sendOne(new SingleMessageSendingRequest(message)); // 메시지 발송 요청
+        } catch (Exception e) {
+            throw new CustomException("인증번호 전송에 실패하였습니다.", HttpStatus.BAD_REQUEST);
+        }
     }
 }
