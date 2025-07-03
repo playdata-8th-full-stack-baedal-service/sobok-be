cb27140851c3f18280c1edbea27a66ddabf6c684
hyeonji <guswl1424@gmail.com>
2025-07-03 11:41:05 +0900

    fix: 식재료 정보 요청 dto에 notBlank 처리

--- cook-service/src/main/java/com/sobok/cookservice/cook/controller/IngredientController.java
+++ cook-service/src/main/java/com/sobok/cookservice/cook/controller/IngredientController.java
@@ -4,11 +4,11 @@ import com.sobok.cookservice.common.dto.ApiResponse;
 import com.sobok.cookservice.common.dto.TokenUserInfo;
 import com.sobok.cookservice.cook.dto.request.IngreReqDto;
 import com.sobok.cookservice.cook.service.IngredientService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,7 +24,7 @@ public class IngredientController {
 
     @PreAuthorize("hasRole('ADMIN')")
     @PostMapping("/ingredient-register")
-    public ResponseEntity<Object> ingreRegister(@RequestBody IngreReqDto reqDto) {
+    public ResponseEntity<Object> ingreRegister(@Valid @RequestBody IngreReqDto reqDto) {
         ingredientService.ingreCreate(reqDto);
         return ResponseEntity.ok().body(ApiResponse.ok(reqDto.getIngreName(), "재료가 등록되었습니다."));
     }
