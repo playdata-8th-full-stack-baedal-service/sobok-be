8b2a4b766b7f3f809a7f1c208094dd7d3995acd5
Jongkyun <hjk326333@gmail.com>
2025-07-03 12:49:29 +0900

    feat: whiteList Url 추가 (permissionNumber 검증 Url)

--- auth-service/src/main/java/com/sobok/authservice/common/security/SecurityConfig.java
+++ auth-service/src/main/java/com/sobok/authservice/common/security/SecurityConfig.java
@@ -35,7 +35,8 @@ public class SecurityConfig {
                                 "/actuator/**", "/auth/login", "/auth/reissue", "/sms/verify",
                                 "/sms/send", "/auth/recover/**", "/auth/user-signup","/auth/rider-signup","/auth/shop-signup",
                                 "/auth/findLoginId", "/auth/verification", "/auth/reset-password",
-                                "/auth/temp-token", "/auth/check-id", "/auth/check-nickname", "/auth/check-email"
+                                "/auth/temp-token", "/auth/check-id", "/auth/check-nickname", "/auth/check-email",
+                                "/auth/check-permission"
                         ).permitAll()
                         .anyRequest().authenticated()
                 )
