c1ff5a7c0252b188f8021bdb540f3defde3e8c26
hyeonji <guswl1424@gmail.com>
2025-07-07 10:42:59 +0900

    feat: 주문하기 전 사용자의 정보 조회 api 구현

---
+++ user-service/src/main/java/com/sobok/userservice/user/dto/request/PreOrderUserReqDto.java
@@ -0,0 +1,13 @@
+package com.sobok.userservice.user.dto.request;
+
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class PreOrderUserReqDto {
+    private Long userId;
+}
