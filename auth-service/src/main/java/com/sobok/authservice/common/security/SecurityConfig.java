b11631bd80eea2ca014ba1b314dd304c934fd7bb
hyeonji <guswl1424@gmail.com>
2025-07-08 09:17:01 +0900

    fix: 카카오 로그인 url 필터 허용

--- auth-service/src/main/java/com/sobok/authservice/common/security/SecurityConfig.java
+++ auth-service/src/main/java/com/sobok/authservice/common/security/SecurityConfig.java
@@ -36,8 +36,7 @@ public class SecurityConfig {
                                 "/sms/send", "/auth/recover/**", "/auth/user-signup", "/auth/rider-signup", "/auth/shop-signup",
                                 "/auth/findLoginId", "/auth/verification", "/auth/reset-password",
                                 "/auth/temp-token", "/auth/check-id", "/auth/check-nickname", "/auth/check-email",
-                                "/auth/check-permission", "/auth/check-shopName", "/auth/check-shopAddress",
-                                "/auth/kakao-login"
+                                "/auth/check-permission", "/auth/check-shopName", "/auth/check-shopAddress"
                         ).permitAll()
                         .anyRequest().authenticated()
                 )
