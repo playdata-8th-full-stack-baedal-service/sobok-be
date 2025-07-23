58aff3a9a8fc9ec6701c102c6eaccca3b26bb2a8
hyeonji <guswl1424@gmail.com>
2025-07-23 14:56:18 +0900

    fix: 비밀번호 변경 시 현재 비밀번호와 검증, 이메일 변경시 형식 검증

--- user-service/src/main/java/com/sobok/userservice/user/dto/email/UserEmailDto.java
+++ user-service/src/main/java/com/sobok/userservice/user/dto/email/UserEmailDto.java
@@ -1,5 +1,6 @@
 package com.sobok.userservice.user.dto.email;
 
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -8,5 +9,9 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class UserEmailDto {
+    @Pattern(
+            regexp = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$",
+            message = "유효한 이메일 형식이어야 합니다."
+    )
     private String email;
 }
