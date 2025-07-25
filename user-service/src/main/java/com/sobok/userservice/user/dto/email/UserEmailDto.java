46c3d0d65b02db251d424cadd161e48bb4ad782e
Jongkyun <hjk326333@gmail.com>
2025-07-25 12:14:57 +0900

    fix: 이메일 변경시 검증 로직 수정

--- user-service/src/main/java/com/sobok/userservice/user/dto/email/UserEmailDto.java
+++ user-service/src/main/java/com/sobok/userservice/user/dto/email/UserEmailDto.java
@@ -10,7 +10,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class UserEmailDto {
     @Pattern(
-            regexp = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$",
+            regexp = "^(?!.*\\.\\.)(?!.*\\.$)(?!^\\.)[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$",
             message = "유효한 이메일 형식이어야 합니다."
     )
     private String email;
