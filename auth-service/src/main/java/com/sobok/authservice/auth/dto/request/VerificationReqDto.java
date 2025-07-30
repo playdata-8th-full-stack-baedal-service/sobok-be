7687ccb026b6f4c265c9c1dbd2ea9fd26e4e9464
Jongkyun <hjk326333@gmail.com>
2025-07-30 11:48:58 +0900

    fix: sms 요청, 인증번호 검증 dto 에서 전화번호 유효성 검증로직 수정

--- auth-service/src/main/java/com/sobok/authservice/auth/dto/request/VerificationReqDto.java
+++ auth-service/src/main/java/com/sobok/authservice/auth/dto/request/VerificationReqDto.java
@@ -1,5 +1,7 @@
 package com.sobok.authservice.auth.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,6 +12,9 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 public class VerificationReqDto {
+
+    @NotBlank(message = "전화번호는 필수 입니다.")
+    @Pattern(regexp = "^01[016789]\\d{8}$", message = "전화번호는 - 없이 숫자만 11자리여야 합니다.")
     private String phoneNumber;
     private String inputCode;
 }
