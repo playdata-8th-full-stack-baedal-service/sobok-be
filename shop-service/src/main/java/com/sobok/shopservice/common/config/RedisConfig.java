de030adaf02789be9ef28acb8417d40b324d85ff
Devy17 <devyks107@gmail.com>
2025-07-30 10:26:06 +0900

    fix : 오류 수정 (redisson)

--- shop-service/src/main/java/com/sobok/shopservice/common/config/RedisConfig.java
+++ shop-service/src/main/java/com/sobok/shopservice/common/config/RedisConfig.java
@@ -6,12 +6,6 @@ import org.redisson.config.Config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
-import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
@@ -19,31 +13,6 @@ public class RedisConfig {
     private String host;
     @Value("${spring.data.redis.port}")
     private int port;
-
-    /**
-     * Connection 객체 생성
-     */
-    @Bean
-    public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory(host, port);
-    }
-
-    /**
-     * 문자열 타입(토큰)을 저장하기 위한 RedisTemplate
-     * Value 직렬화 - StringRedisSerializer
-     */
-    @Bean @Primary
-    public RedisTemplate<String, String> redisStringTemplate(RedisConnectionFactory redisConnectionFactory) {
-        // 템플릿 생성
-        RedisTemplate<String, String> template = new RedisTemplate<>();
-        template.setConnectionFactory(redisConnectionFactory);
-
-        // 직렬화 도구 설정
-        template.setKeySerializer(new StringRedisSerializer());
-        template.setValueSerializer(new StringRedisSerializer());
-
-        return template;
-    }
 
     @Bean(destroyMethod = "shutdown")
     public RedissonClient redissonClient() {
