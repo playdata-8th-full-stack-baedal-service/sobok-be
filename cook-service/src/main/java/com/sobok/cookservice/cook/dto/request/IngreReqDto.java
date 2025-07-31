33a9326db3c08e2e414f13e0d6b5b2539f9a33ea
Jongkyun <hjk326333@gmail.com>
2025-07-31 12:28:34 +0900

    fix: 식재료, 요리등록 가격 단위 음수값 입력 안되게 수정

--- cook-service/src/main/java/com/sobok/cookservice/cook/dto/request/IngreReqDto.java
+++ cook-service/src/main/java/com/sobok/cookservice/cook/dto/request/IngreReqDto.java
@@ -1,5 +1,6 @@
 package com.sobok.cookservice.cook.dto.request;
 
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.*;
@@ -18,12 +19,14 @@ public class IngreReqDto {
     private String ingreName;
 
     @NotNull
+    @Min(value = 0, message = "가격은 0 이상이어야 합니다.")
     private Integer price;
 
     @NotBlank(message = "식재료 원산지는 필수입니다.")
     private String origin;
 
     @NotNull
+    @Min(value = 1, message = "단위는 1 이상이어야 합니다.")
     private Integer unit;
 
 }
