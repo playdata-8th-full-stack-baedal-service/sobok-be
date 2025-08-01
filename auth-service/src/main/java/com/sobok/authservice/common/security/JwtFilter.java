822fc1a0a7e83726c9d04f270c43fef241d2d554
Jongkyun <hjk326333@gmail.com>
2025-08-01 17:20:53 +0900

    fix: 가게 회원가입 role 설정

--- auth-service/src/main/java/com/sobok/authservice/common/security/JwtFilter.java
+++ auth-service/src/main/java/com/sobok/authservice/common/security/JwtFilter.java
@@ -37,7 +37,7 @@ public class JwtFilter extends OncePerRequestFilter {
     private final RedisTemplate<String, String> redisTemplate;
     List<String> whiteList = List.of(
             "/actuator/**", "/auth/login", "/auth/reissue", "/sms/send", "/auth/recover/**", "/sms/verify",
-            "/auth/user-signup", "/auth/rider-signup", "/auth/shop-signup", "/auth/findLoginId", "/auth/verification", "/auth/reset-password",
+            "/auth/user-signup", "/auth/rider-signup", "/auth/findLoginId", "/auth/verification", "/auth/reset-password",
             "/auth/temp-token", "/auth/check-id", "/auth/check-nickname", "/auth/check-email", "/auth/check-permission", "/auth/check-shopName",
             "/auth/check-shopAddress", "/auth/social-user-signup", "/v3/**"
     );
