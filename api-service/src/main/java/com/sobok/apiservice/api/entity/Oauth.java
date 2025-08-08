2caba6f7ad2a5b69ab48e345493e468b674d0564
Devy <devyks107@gmail.com>
2025-08-08 10:02:00 +0900

    v1.3.2
    * v1.3.0
    
    * fix : Cook recipe XSS 공격 방어
    
    * fix : redirect uri 수정

--- api-service/src/main/java/com/sobok/apiservice/api/entity/Oauth.java
+++ api-service/src/main/java/com/sobok/apiservice/api/entity/Oauth.java
@@ -15,9 +15,9 @@ public class Oauth {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(unique = true)
+    @Column(nullable = false, unique = true)
     private String socialId;
 
-    @Column(unique = true)
+    @Column(nullable = false)
     private String socialProvider; // GOOGLE, KAKAO, NAVER, null
 }
